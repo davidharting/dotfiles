ed3a0094766cdd68acf1dd55f7cd135ec9487a8d
David Harting <hartingdavid@icloud.com>
2024-03-04 22:36:36 -0500

    Use ruff fmt when available

--- nvim/init.lua
+++ nvim/init.lua
@@ -549,8 +549,14 @@ require('lazy').setup {
       },
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        python = { { 'black', 'ruff' } },
+
+        python = function(bufnr)
+          if require('conform').get_formatter_info('ruff_format', bufnr).available then
+            return { 'ruff_format' }
+          else
+            return { 'black' }
+          end
+        end,
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
