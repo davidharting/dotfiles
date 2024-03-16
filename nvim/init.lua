f0f68d61d6f8fc68bfe6d75bb7faf66c6705e3d1
David Harting <hartingdavid@icloud.com>
2024-03-16 17:39:02 -0400

    Add hotkey for toggling per-line blame ghost text

--- nvim/init.lua
+++ nvim/init.lua
@@ -183,6 +183,17 @@ require('lazy').setup {
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+        map('n', '<leader>hb', function()
+          gs.blame_line { full = true }
+        end, { desc = 'Toggle blame for buffer' })
+      end,
     },
   },
 
