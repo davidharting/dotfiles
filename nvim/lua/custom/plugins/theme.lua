e197042afb89a23d0255e7d823164fefc1a85a83
David Harting <hartingdavid@icloud.com>
2024-03-23 22:44:26 -0400

    Put nvim theme in own file

---
+++ nvim/lua/custom/plugins/theme.lua
@@ -0,0 +1,35 @@
+return {
+  -- Theme replicating VSCode Default
+  'Mofiqul/vscode.nvim',
+  priority = 1000,
+  lazy = false,
+  config = function()
+    local c = require('vscode.colors').get_colors()
+    require('vscode').setup {
+      -- Enable transparent background
+      -- transparent = true,
+
+      -- Enable italic comment
+      italic_comments = true,
+
+      -- Disable nvim-tree background color
+      disable_nvimtree_bg = true,
+
+      -- Override colors (see ./lua/vscode/colors.lua)
+      color_overrides = {
+        -- vscLineNumber = '#FFFFFF',
+      },
+
+      -- Override highlight groups (see ./lua/vscode/theme.lua)
+      group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+        Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = false },
+      },
+    }
+    require('vscode').load()
+  end,
+}
+-- return { 'EdenEast/nightfox.nvim' }
+-- return { 'bluz71/vim-moonfly-colors', name = 'moonfly', lazy = false, priority = 1000 }
+-- return { 'sainnhe/everforest', name = 'everforest', lazy = false, priority = 1000 }
