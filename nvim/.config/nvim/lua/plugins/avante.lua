7d72584507516769d1532b04591baa2d8e297e27
David Harting <hartingdavid@icloud.com>
2025-10-12 09:46:15 -0400

    disable avante

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,49 @@
+return {}
+-- return {
+--   "yetone/avante.nvim",
+--   event = "VeryLazy",
+--   version = false,
+--   --@module 'avante'
+--   ---@type avante.Config
+--   opts = {
+--     provider = "copilot",
+--   },
+--   build = "make",
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     --- The below dependencies are optional,
+--     "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+--     "ibhagwan/fzf-lua", -- for file_selector provider fzf
+--     "stevearc/dressing.nvim", -- for input provider dressing
+--     "folke/snacks.nvim", -- for input provider snacks
+--     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--     "zbirenbaum/copilot.lua", -- for providers='copilot'
+--     {
+--       -- support for image pasting
+--       "HakonHarnes/img-clip.nvim",
+--       event = "VeryLazy",
+--       opts = {
+--         -- recommended settings
+--         default = {
+--           embed_image_as_base64 = false,
+--           prompt_for_file_name = false,
+--           drag_and_drop = {
+--             insert_mode = true,
+--           },
+--           -- required for Windows users
+--           -- use_absolute_path = true,
+--         },
+--       },
+--     },
+--     {
+--       -- Make sure to set this up properly if you have lazy=true
+--       "MeanderingProgrammer/render-markdown.nvim",
+--       opts = {
+--         file_types = { "markdown", "Avante" },
+--       },
+--       ft = { "markdown", "Avante" },
+--     },
+--   },
+-- }
