e7520bf6da3ff64d6f6aed025a7f851514669a48
David Harting <hartingdavid@icloud.com>
2026-03-27 10:07:31 -0400

    Reveal all MD characters in vim
    the auto hiding of backtick fences is driving me nuts

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,10 @@
 vim.opt.relativenumber = false
 
 vim.g.lazyvim_python_lsp = "ty"
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
